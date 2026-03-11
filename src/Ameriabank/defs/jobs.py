04d38d00b00890badb10faf99a6ffb0be2f4523f
arin_quantori <arin.avanoosyan@quantori.academy>
2026-03-11 15:59:07 +0400

    jobs partitions fixed

--- src/Ameriabank/defs/jobs.py
+++ src/Ameriabank/defs/jobs.py
@@ -2,9 +2,7 @@ import dagster as dg
 from .assets.assets import daily_partition_def
 fetch_daily_data = dg.AssetSelection.assets(["fetch_daily_data"])
 daily_job = dg.define_asset_job("daily_job",
- selection = dg.AssetSelection.assets("trx", "fetch_daily_data"),
- partitions_def = daily_partition_def
- 
+ selection = dg.AssetSelection.assets("trx", "fetch_daily_data") 
 )
 other_job = dg.define_asset_job("other_job", 
                                 selection = dg.AssetSelection.all()-fetch_daily_data)
